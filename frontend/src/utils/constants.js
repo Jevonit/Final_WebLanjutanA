3e02379cfa54cf16ff7b964ac28509943ad74453
jevonit <jevonit252@gmail.com>
2025-06-22 23:38:08 +0800

    Fix Deploy Problem 7

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 // This file exports constants used throughout the application.
 
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://finalweblanjutana-production.up.railway.app";
 export const JOB_POSTS_PER_PAGE = 10;
 export const APPLICATION_STATUS = {
     PENDING: "Pending",
